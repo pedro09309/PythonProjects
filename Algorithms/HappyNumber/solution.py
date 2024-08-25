dffc6a9a1c4410941ae101151635c21bf04322c9
DESEPED <DESEPED@hilti.com>
2024-08-25 20:14:16 +0200

    added new algorithms

--- Algorithms/HappyNumber/solution.py
+++ Algorithms/HappyNumber/solution.py
@@ -2,14 +2,13 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         res = 0
 
-        for i in range(n):
+        while n > 0:
             res += (n % 10)**2
             n = (n // 10)
         
-        if res == 1     : return True
-        elif (res >= 10): return self.isHappy(res)
-        else            : return False
-
+        if (res == 1)  or (res == 7): return True
+        elif (res < 10)             : return False
+        else                        : return self.isHappy(res)
 
 # TEST CASE
 solution = Solution()
